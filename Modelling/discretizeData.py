2f423ba34a83c6ff5a65fde2d600ba66ef1a8393
TommyGun2812 <perezveratomas08@gmail.com>
2025-10-22 11:21:14 -0600

    feat: discretize and limit model output

---
+++ Modelling/discretizeData.py
@@ -0,0 +1,26 @@
+# =============================================================
+# Nombre del archivo: discretizeData.py
+# Autor: Tomás Pérez Vera
+# Fecha de creación: 22-10-2025
+# Descripción: Function para discretizar salida del modelo
+# Dependencias: Ninguna
+# =============================================================
+
+def discretize_value(value: float) -> float:
+    """
+    Función para redondear al múltiplo de 0.25 más cercano 
+
+    Args: 
+        value (float):  Valor de punto flotante
+
+    Return: 
+        _ (float): Valor de punto flotante discretizado
+
+    """
+
+    if value > 5.0: 
+        value = 5.0
+    elif value < 1.0: 
+        value = 1.0
+
+    return round(value * 4) / 4
