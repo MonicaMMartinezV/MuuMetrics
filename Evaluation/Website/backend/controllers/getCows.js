61134c74db5073a0023626b7c92ae315656b48a0
Maria José Soto <A01705840@tec.mx>
2025-11-27 01:29:41 -0600

    feat: lista de ids vaca en interfaz

--- Evaluation/Website/backend/controllers/getCows.js
+++ Evaluation/Website/backend/controllers/getCows.js
@@ -1,21 +1,31 @@
-const cowInfo = require('../models/Cow');
+const driveService = require('../models/driveServices.js');
+const path = require('path');
 
-exports.getCows = async (req, res) => {
+
+/**
+ * Handles the main request to find, download, and process cow data. (Route /cow-data)
+ */
+exports.getCowData = async (req, res) => {
     try {
+        const files = await driveService.getFiles();
 
-        const cows = [
-        { IDCow: 1101 }, 
-        { IDCow: 2105 }, 
-        { IDCow: 3133 },
-        { IDCow: 9199 }];
+        // Get all files whose filename begins with 4 digits and are images
+        const cowFiles = files.filter(f =>
+            /^[0-9]{4}/.test(f.name) && f.mimeType.startsWith("image/")
+        );
 
-        //const cows = await cowInfo.findAll();
+        // Extract unique cow IDs
+        const uniqueCowIds = [...new Set(
+            cowFiles.map(f => f.name.substring(0, 4))
+        )];
 
-        res.render("cows", {
-            cows
+        res.render("cows", {   
+            filesFound: cowFiles.length,
+            cows: uniqueCowIds.map(id => ({ IDCow: id }))
         });
+
     } catch (error) {
-        console.error(error);
-        res.status(500).send("Error refreshing cow list");
-    }
+        console.error("Error:", error);
+        res.status(500).json({ error: error.message });
+}
 };
